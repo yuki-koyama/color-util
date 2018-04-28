71cb3efa85fed59b2fc62d9f7a11b9c12870be65
Yuki Koyama <yuki@koyama.xyz>
2018-04-27 23:10:43 -0400

    Add a function definition for CIEDE2000

--- color-util/CIEDE2000.hpp
+++ color-util/CIEDE2000.hpp
@@ -1,9 +1,15 @@
 #ifndef CIEDE2000_hpp
 #define CIEDE2000_hpp
 
+#include <color-util/type.hpp>
+
 namespace colorutil
 {
-    
+    inline double CalculateCIEDE2000(const Lab& color_0, const Lab& color_1)
+    {
+		// TODO: Implement distance calculation    	
+        return 0.0;
+    }
 }
 
 #endif /* CIEDE2000_hpp */
