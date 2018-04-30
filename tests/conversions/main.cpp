8a1783045196e19f4d3703f2482ed13449e73696
Yuki Koyama <yuki@koyama.xyz>
2018-04-30 16:44:27 +0900

    Add conversion tests

--- tests/conversions/main.cpp
+++ tests/conversions/main.cpp
@@ -4,5 +4,13 @@
 
 int main()
 {
+    colorutil::RGB rgb_color(200.0 / 255.0, 100.0 / 255.0, 20.0 / 200.0);
+    colorutil::XYZ xyz_color = colorutil::convert_RGB_to_XYZ(rgb_color);
+    colorutil::Lab lab_color = colorutil::convert_XYZ_to_Lab(xyz_color);
+    
+    std::cout << "sRGB   : " << rgb_color.transpose() << std::endl;
+    std::cout << "CIEXYZ : " << xyz_color.transpose() << std::endl;
+    std::cout << "CIELAB : " << lab_color.transpose() << std::endl;
+    
     return 0;
 }
