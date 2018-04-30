1d304f8204e70dc7be8de1b98d6821a79241d919
Yuki Koyama <yuki@koyama.xyz>
2018-04-30 15:35:59 +0900

    Implement XYZ-to-Lab conversion and close #3

--- color-util/XYZ_to_Lab.hpp
+++ color-util/XYZ_to_Lab.hpp
@@ -5,9 +5,41 @@
 #ifndef XYZ_to_Lab_h
 #define XYZ_to_Lab_h
 
+#include <cmath>
+#include <color-util/type.hpp>
+
 namespace colorutil
 {
-
+    ///
+    /// \brief Convert a CIEXYZ color into a CIELAB color under Illuminant D65.
+    /// \param xyz_color A color represented in CIEXYZ
+    /// \return A color represented in CIELAB
+    ///
+    inline Lab convert_XYZ_to_Lab(const XYZ& xyz_color)
+    {
+        // The reference white point under Illuminant D65
+        const XYZ reference_xyz(95.047, 100.000, 108.883);
+        
+        auto f = [](double t)
+        {
+            constexpr double delta = 6.0 / 29.0;
+            if (t > delta * delta * delta)
+            {
+                return std::pow(t, 1.0 / 3.0);
+            }
+            else
+            {
+                return t / (delta * delta) + 4.0 / 29.0;
+            }
+        };
+        
+        const double L = 116.0 * f(xyz_color(1) / reference_xyz(1)) - 16.0;
+        const double a = 500.0 * (f(xyz_color(0) / reference_xyz(0)) - f(xyz_color(1) / reference_xyz(1)));
+        const double b = 200.0 * (f(xyz_color(1) / reference_xyz(1)) - f(xyz_color(2) / reference_xyz(2)));
+        
+        return Lab(L, a, b);
+    }
 }
+
 
 #endif /* XYZ_to_Lab_h */
--- tests/conversions/main.cpp
+++ tests/conversions/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <color-util/RGB_to_XYZ.hpp>
+#include <color-util/XYZ_to_Lab.hpp>
 
 int main()
 {
