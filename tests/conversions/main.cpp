7b6065c5a36bbe639d1a65955daf6f60093045d0
Yuki Koyama <yuki@koyama.xyz>
2018-05-11 09:04:16 +0900

    Add HSL_to_RGB conversion and close #10

--- tests/conversions/main.cpp
+++ tests/conversions/main.cpp
@@ -2,10 +2,11 @@
 #include <color-util/RGB_to_XYZ.hpp>
 #include <color-util/XYZ_to_Lab.hpp>
 #include <color-util/RGB_to_HSL.hpp>
+#include <color-util/HSL_to_RGB.hpp>
 
 int main()
 {
-    colorutil::RGB rgb_color(200.0 / 255.0, 100.0 / 255.0, 20.0 / 200.0);
+    colorutil::RGB rgb_color(200.0 / 255.0, 100.0 / 255.0, 20.0 / 255.0);
     colorutil::XYZ xyz_color = colorutil::convert_RGB_to_XYZ(rgb_color);
     colorutil::Lab lab_color = colorutil::convert_XYZ_to_Lab(xyz_color);
     colorutil::HSL hsl_color = colorutil::convert_RGB_to_HSL(rgb_color);
@@ -14,6 +15,9 @@ int main()
     std::cout << "HSL    : " << hsl_color.transpose() << std::endl;
     std::cout << "CIEXYZ : " << xyz_color.transpose() << std::endl;
     std::cout << "CIELAB : " << lab_color.transpose() << std::endl;
+    
+    // Test the correctness of the RGB => HSL => RGB conversion
+    assert((rgb_color - colorutil::convert_HSL_to_RGB(hsl_color)).norm() < 1e-5);
     
     return 0;
 }
