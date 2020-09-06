3690f4d3af677a0c9ea2e1bf037bafcb36ca1948
Yuki Koyama <yuki@koyama.xyz>
2020-09-06 19:13:22 +0900

    Refactor

--- tests/conversions/main.cpp
+++ tests/conversions/main.cpp
@@ -15,16 +15,17 @@
 
 int main()
 {
-    colorutil::RGB rgb_color(200.0 / 255.0, 100.0 / 255.0, 20.0 / 255.0);
-    colorutil::XYZ xyz_color = colorutil::convert_RGB_to_XYZ(rgb_color);
-    colorutil::Lab lab_color = colorutil::convert_XYZ_to_Lab(xyz_color);
-    colorutil::HSL hsl_color = colorutil::convert_RGB_to_HSL(rgb_color);
-    double delta;
+    const colorutil::RGB rgb_color(200.0 / 255.0, 100.0 / 255.0, 20.0 / 255.0);
+    const colorutil::XYZ xyz_color = colorutil::convert_RGB_to_XYZ(rgb_color);
+    const colorutil::Lab lab_color = colorutil::convert_XYZ_to_Lab(xyz_color);
+    const colorutil::HSL hsl_color = colorutil::convert_RGB_to_HSL(rgb_color);
 
     std::cout << "sRGB   : " << rgb_color.transpose() << std::endl;
     std::cout << "HSL    : " << hsl_color.transpose() << std::endl;
     std::cout << "CIEXYZ : " << xyz_color.transpose() << std::endl;
     std::cout << "CIELAB : " << lab_color.transpose() << std::endl;
+
+    double delta;
 
     // Test the correctness of the RGB => HSL => RGB conversion
     if ((delta = (rgb_color - colorutil::convert_HSL_to_RGB(hsl_color)).norm()) > 1e-5)
